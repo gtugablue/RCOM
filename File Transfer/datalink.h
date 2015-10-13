42ca17639ef5c7f590c16e654a8f759ea2503d0d
aslmbc13 <asl_mbc_13@hotmail.com>
2015-10-13 17:42:16 +0100

    Function headers

--- File Transfer/datalink.h
+++ File Transfer/datalink.h
@@ -5,19 +5,36 @@
  * Describe function return values
  */
 #define OK 0
-#define ERROR 1
+#define ERROR -1
 
 /*
  * Define the program mode (either reader or writer)
  */
-#define LLOPEN_READ 0
-#define LLOPEN_WRITE 1
+#define TRANSMITTER 0
+#define RECEIVER 1
 
 /*
  * Starts the connection via serial-port, allowing for it to be either reader or writer
+ * Returns port fd, -1 if error
  */
-int llopen(int port_fd, int mode);
+int llopen(int porta, int mode);
 
+/*
+ * Writes length bytes from buffer to fd
+ * Return number of bytes written on success, -1 if error
+ */
+int llwrite(int fd, char * buffer, int length);
 
+/*
+ * Reads from fd to buffer
+ * Returns buffer size if ok, -1 if error
+ */
+int llread(int fd, char * buffer);
+
+/*
+ * Closes fd data link
+ * Returns >0 if success, <0 on error
+ */
+int llclose(int fd);
 
 #endif
