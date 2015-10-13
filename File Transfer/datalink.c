2c8dfd865a6a9881b6ad6ee9543d5aca35eaa302
aslmbc13 <asl_mbc_13@hotmail.com>
2015-10-13 18:55:53 +0100

    State machine prototype

--- File Transfer/datalink.c
+++ File Transfer/datalink.c
@@ -16,10 +16,10 @@ int llclose(int fd) {
 
 }
 
-char* get_packet(int fd) {
+char* get_frame(int fd) {
 	state_t state = START;
 	unsigned char byte;
-	packet_t packet;
+	frame_t frame;
 
 	while(state != STOP) {
 		int ret = read_byte(fd, &byte);
@@ -30,15 +30,89 @@ char* get_packet(int fd) {
 
 		switch(state) {
 		case START:
-
+			if(byte == FLAG) {
+				state = FLAG_RCV;
+				frame.buffer[frame.length++] = byte;
+			}
 			break;
+		case FLAG_RCV:
+			if(byte == A_TRANSMITTER || byte == A_RECEIVER) {	// TODO check
+				state = A_RCV;
+				frame.buffer[frame.length++] = byte;
+			} else if(byte != FLAG) {
+				state = START;
+				frame.length = 0;
+			}
+			break;
+		case A_RCV:
+			if(byte == flag) {
+				frame.length = 1;
+				state = FLAG_RCV;
+			} else if(byte == C_SET) {
+				frame.type = SET;
+				frame.buffer[frame.length++] = byte;
+				state = C_RCV;
+			} else if(byte == C_UA) {
+				frame.type = UA;
+				frame.buffer[frame.length++] = byte;
+				state = C_RCV;
+			} else if(byte == C_INFO(0)  || byte == C_INFO(1)) {
+				frame.type = DATA;
+				frame.buffer[frame.length++] = byte;
+				state = C_RCV;
+			} else {
+				frame.length = 0;
+				state = START;
+			}
+			break;
+		case C_RCV:
+			if(byte == frame.buffer[1] ^ frame.buffer[2]) {
+				if(frame.type == SET || frame.type == UA)
+					state = BCC1_RCV;
+				else
+					state = DATA_ESC_RCV;
+
+				frame.buffer[frame.length++] = byte;
+			} else if (byte == FLAG){
+				state = FLAG_RCV;
+				frame.length = 1;
+			} else {
+				frame.length = 0;
+				state = START;
+			}
+			break;
+		case BCC1_RCV:
+			if(byte == FLAG) {
+				frame.buffer[frame.length++] = byte;
+				state = STOP;
+			} else {
+				frame.length = 0;
+				state = START;
+			}
+			break;
+		case DATA_ESC_RCV:
+			if(byte == ESC) {
+				state = DATA_RCV;
+			} else if(byte == FLAG) {
+				frame.buffer[frame.length++] = byte;
+				if(bcc2_checks(&frame)) {
+					state = stop;
+				}
+			} else {
+				frame.buffer[frame.length++] = byte;
+			}
+		case DATA_RCV:
+			frame.buffer[frame.length++] = byte;
+			state = DATA_ESC_RCV;
 		case STOP:
 			break;
 		}
 	}
+
+	return frame;
 }
 
-/*int read_packet(int fd, unsigned char C) // TODO
+/*int read_frame(int fd, unsigned char C) // TODO
 {
 	state_t state = START;
 	unsigned char byte;
