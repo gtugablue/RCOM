2298c62e66237efe6f11e93f2ad7b7ea4bd8f472
aslmbc13 <asl_mbc_13@hotmail.com>
2015-10-13 17:37:21 +0100

    llopen

--- File Transfer/datalink.h
+++ File Transfer/datalink.h
@@ -1,6 +1,23 @@
 #ifndef DATALINK_H
 #define DATALINK_H
 
+/*
+ * Describe function return values
+ */
+#define OK 0
+#define ERROR 1
+
+/*
+ * Define the program mode (either reader or writer)
+ */
+#define LLOPEN_READ 0
+#define LLOPEN_WRITE 1
+
+/*
+ * Starts the connection via serial-port, allowing for it to be either reader or writer
+ */
+int llopen(int port_fd, int mode);
+
 
 
 #endif
