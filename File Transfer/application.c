aebf0ce336a63bdd8de462ebb6091e542453c683
gtugablue <gtugablue@gmail.com>
2015-10-25 17:36:10 +0000

    Application stuff.

--- File Transfer/application.c
+++ File Transfer/application.c
@@ -1,7 +1,33 @@
 #include "application.h"
 #include <stdio.h>
+#include <stdint.h>
 
 #define MAX(A, B) (((A) > (B)) ? (A) : (B))
+
+typedef enum {
+	PACKET_CTRL_FIELD_DATA = 0,
+	PACKET_CTRL_FIELD_START = 1,
+	PACKET_CTRL_FIELD_END = 2
+} packet_ctrl_field_t;
+
+typedef struct {
+	unsigned char type;
+	unsigned char length;
+	unsigned char *value;
+} control_packet_param_t;
+
+typedef struct {
+	unsigned char ctrl_field;
+	unsigned char sn;
+	uint16_t length;
+	unsigned char *data;
+} data_packet_t;
+
+typedef struct {
+	unsigned char ctrl_field;
+	unsigned num_params;
+	control_packet_param_t *params;
+} control_packet_t;
 
 int main(int argc, char *argv[]) // ./file_transfer <port> <send|receive> <filename>
 {
@@ -36,9 +62,16 @@ int send_file(const unsigned char *port, const unsigned char *file_name)
 	return llclose(fd);
 }
 
-int send_packet(int fd, const unsigned char *data, unsigned size)
+int send_data_packet(int fd, const data_packet_t *data_packet)
 {
-	if (llwrite(fd, &data[i], size) < 0) return 1;
+	unsigned size = data_packet->length + 4;
+	unsigned char packet[size];
+	packet[0] = data_packet->control_field;
+	packet[1] = data_packet->sn;
+	packet[2] = data_packet->length & 0xFF00;
+	packet[3] = data_packet->length & 0x00FF;
+	memcpy(&packet[4], data_packet->data, length);
+	if (llwrite(fd, packet, size) < 0) return 1;
 	// TODO
 }
 
