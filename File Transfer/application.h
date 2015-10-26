a0a76967c2a89f00334637a3545cb5aa31918660
Andre <andrelago1995@gmail.com>
2015-10-26 23:57:16 +0000

    shrink

--- File Transfer/application.h
+++ File Transfer/application.h
@@ -1,7 +1,7 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
 
-#define MAX_PACKET_SIZE	24
+#define MAX_PACKET_SIZE	4
 
 int send_file(const char *port, const char *file_name);
 int receive_file(const char *port, const char *destination_folder);
