1b8b337bb36e5d037fa2dd3c78fea5d28f46152a
root <root@tux33.netlab.fe.up.pt>
2015-10-27 04:53:04 +0000

    1024

--- File Transfer/application.h
+++ File Transfer/application.h
@@ -1,7 +1,7 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
 
-#define MAX_PACKET_SIZE	124
+#define MAX_PACKET_SIZE	1024
 
 int send_file(const char *port, const char *file_name);
 int receive_file(const char *port, const char *destination_folder);
