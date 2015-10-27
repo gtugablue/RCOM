e659512bd6752463f7dfb3da965ca95fd47cba36
Andre <andrelago1995@gmail.com>
2015-10-27 00:14:44 +0000

    vcghv

--- File Transfer/application.h
+++ File Transfer/application.h
@@ -1,7 +1,7 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
 
-#define MAX_PACKET_SIZE	4
+#define MAX_PACKET_SIZE	20
 
 int send_file(const char *port, const char *file_name);
 int receive_file(const char *port, const char *destination_folder);
