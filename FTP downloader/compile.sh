80311f3c92f9c4383b34600134ac315ffedb464c
Gustavo Rocha da Silva <silva95gustavo@gmail.com>
2015-11-16 16:52:48 +0000

    ...

---
+++ FTP downloader/compile.sh
@@ -0,0 +1,1 @@
+gcc -Wall downloader.c -o downloader
--- FTP downloader/downloader.c
+++ FTP downloader/downloader.c
@@ -1,7 +1,47 @@
 #include <regex.h>
+#include <stdio.h>
+#include <string.h>
 
 int main()
 {
-	// Regex: ftp:\/\/((?:(.*):(.*)@)?(.*))\/(.*)
+	char source[] = "ftp://fe.up.pt/dir";
+	char regexString[] = "^ftp://((.*):(.*)@)?(.+)/(.*)$";
+
+	regex_t regex;
+
+	size_t maxGroups = 10;
+	regmatch_t groupArray[maxGroups];
+
+	if (regcomp(&regex, regexString, REG_EXTENDED))
+	{
+		printf("Error: could not compile regex.\n");
+		return 1;
+	}
+	else
+		printf("Regex compiled successfully.\n");
+
+	if (regexec(&regex, source, maxGroups, groupArray, 0) == 0)
+	{
+		printf("Regex validated.\n");
+
+		unsigned int g = 0;
+		for (g = 0; g < maxGroups; g++)
+		{
+			if (groupArray[g].rm_so == (size_t)-1)
+				break; // No more groups
+
+			char sourceCopy[strlen(source) + 1];
+			strcpy(sourceCopy, source);
+			sourceCopy[groupArray[g].rm_eo] = 0;
+			printf("Group %u: [%2u-%2u]: %s\n",
+					g, groupArray[g].rm_so, groupArray[g].rm_eo,
+					sourceCopy + groupArray[g].rm_so);
+		}
+	}
+	else
+		printf("Regex validation failed.\n");
+
+	regfree(&regex);
+
 	return 0;
 }
