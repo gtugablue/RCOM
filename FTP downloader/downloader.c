0981c32fc0aefae416c5a8bfff43c5a02b76c55b
Gustavo Rocha da Silva <gtugablue@gmail.com>
2015-11-22 18:50:57 +0000

    URL validation and parsing done.

--- FTP downloader/downloader.c
+++ FTP downloader/downloader.c
@@ -1,61 +1,132 @@
 #include <regex.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-int main()
+bool validateURL(const char *url);
+int parseURL(const char *url, char **user, char **pass, char **host, char **dir);
+
+int main(int argc, char *argv[])
 {
-	char source[] = "ftp://fe.up.pt/dir";
-	char regexString[] = "^ftp://"
+	if (argc != 2)
+	{
+		printf("Usage: %s <url>\n", argv[0]);
+		return 1;
+	}
+	if (!validateURL(argv[1])) return 1;
+
+	char *user;
+	char *pass;
+	char *host;
+	char *dir;
+	parseURL(argv[1], &user, &pass, &host, &dir);
+	free(user);
+	free(pass);
+	free(host);
+	free(dir);
+
+	return 0;
+}
+
+bool validateURL(const char *url)
+{
+	char regexString[] = "^ftp://"		// ftp://
 			"("
 			"("
-			"([^:@/]*)"
-			"(:"
-			"([^:@/]*)"
+			"("
+			"[^:@/]*"		// <user>
+			")"
+			"("
+			":"				// :
+			"("
+			"[^:@/]*"	// <pass>
+			")"
 			")?"
 			")?"
-			"@"
+			"@"						// @
 			")?"
-			"([^:@/]+)"
-			"/"
-			"(.*)"
+			"("
+			"[^:@/]+"				// <host>
+			")"
+			"/"							// /
+			"(.*)"						// <dir>
 			"$";
 
 	regex_t regex;
-
-	size_t maxGroups = 10;
-	regmatch_t groupArray[maxGroups];
 
 	int res;
 	if ((res = regcomp(&regex, regexString, REG_EXTENDED)))
 	{
 		printf("Error #%d compiling regex.\n", res);
-		return 1;
+		return false;
 	}
 	else
 		printf("Regex compiled successfully.\n");
 
-	if (regexec(&regex, source, maxGroups, groupArray, 0) == 0)
+	if (regexec(&regex, url, 0, NULL, 0) == 0)
 	{
 		printf("Regex validated.\n");
-
-		unsigned int g = 0;
-		for (g = 0; g < maxGroups; g++)
-		{
-			if (groupArray[g].rm_so == (size_t)-1)
-				break; // No more groups
-
-			char sourceCopy[strlen(source) + 1];
-			strcpy(sourceCopy, source);
-			sourceCopy[groupArray[g].rm_eo] = 0;
-			printf("Group %u: [%2u-%2u]: %s\n",
-					g, groupArray[g].rm_so, groupArray[g].rm_eo,
-					sourceCopy + groupArray[g].rm_so);
-		}
+		regfree(&regex);
+		return true;
 	}
 	else
+	{
 		printf("Regex validation failed.\n");
+		regfree(&regex);
+		return false;
+	}
+}
 
-	regfree(&regex);
+int parseURL(const char *url, char **user, char **pass, char **host, char **dir)
+{
+	const char *temp = url;
+	if ((temp = strchr(temp, '/')) == NULL) return 1;
+	if ((temp = strchr(++temp, '/')) == NULL) return 1;
+
+	unsigned length;
+	char *atSign = strchr(++temp, '@');
+	if (atSign == NULL)
+	{
+		*user = NULL;
+		*pass = NULL;
+	}
+	else
+	{
+		char *colon = strchr(temp, ':');
+		length = (colon == NULL ? atSign : colon) - temp;
+		if ((*user = malloc((length + 1) * sizeof(char))) == NULL) return 1;
+		memcpy(*user, temp, length);
+		*(*user + length) = '\0';
+
+		if ((colon != NULL && colon < atSign)) // Has password
+		{
+			temp = colon + 1;
+			length = atSign - temp;
+			if ((*pass = malloc((length + 1) * sizeof(char))) == NULL) return 1;
+			memcpy(*pass, temp, length);
+			*(*pass + length) = '\0';
+		}
+		else *pass = NULL;
+	}
+
+	temp = (atSign == NULL ? temp : atSign + 1);
+	char *dir_slash = strchr(temp, '/');
+	length = dir_slash - temp;
+	if ((*host = malloc((length + 1) * sizeof(char))) == NULL) return 1;
+	memcpy(*host, temp, length);
+	*(*host + length) = '\0';
+
+	temp = dir_slash + 1;
+	length = strlen(temp);
+	if ((*dir = malloc((length + 1) * sizeof(char))) == NULL) return 1;
+	memcpy(*dir, temp, length);
+	*(*dir + length) = '\0';
+
+	if (*user != NULL) printf("User: %s\n", *user);
+	if (*pass != NULL) printf("Pass: %s\n", *pass);
+	if (*host != NULL) printf("Host: %s\n", *host);
+	if (*dir != NULL) printf("Dir: %s\n", *dir);
 
 	return 0;
 }
