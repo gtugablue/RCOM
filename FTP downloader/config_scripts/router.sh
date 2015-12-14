6aaf5ec10e3e9c91cf7ea40f54675bfac285385e
aslmbc13 <asl_mbc_13@hotmail.com>
2015-12-14 01:51:01 +0000

    Fix

--- FTP downloader/config_scripts/router.sh
+++ FTP downloader/config_scripts/router.sh
@@ -7,6 +7,15 @@ copy running-config flash:<turna-nome1-nome2-nome3>
 #reiniciar usando mesmos passos mas novo ficheiro em vez de tuxy-clean
 
 #Experiment 4
+#	SEM NAT
+conf t
+interface gigabitethernet 0/0
+ip address 172.16.61.254 255.255.255.0
+no shutdown
+exit
+end
+
+#	COM NAT
 conf t
 interface gigabitethernet 0/0
 ip address 172.16.<BANCADA>1.254 255.255.255.0
@@ -15,21 +24,18 @@ ip nat inside
 exit
 
 interface gigabitethernet 0/1
-ip address 172.16.1.19 255.255.255.0
+ip address 172.16.1.<BANCADA>9 255.255.255.0
 no shutdown
 ip nat outside
 exit
 
-ip nat pool ovrld 172.16.1.19 172.16.1.19 prefix 24
+ip nat pool ovrld 172.16.1.<BANCADA>9 172.16.1.<BANCADA>9 prefix 24
 ip nat inside source list 1 pool ovrld overload
 
+#permite acesso de ip's menores que 7 (tux 1 e 2)
 access-list 1 permit 172.16.<BANCADA>0.0 0.0.0.7
 access-list 1 permit 172.16.<BANCADA>1.0 0.0.0.7
 
-ip route 0.0.0.0 0.0.0.0 172.16.<BANCADA>1.254
+ip route 0.0.0.0 0.0.0.0 172.16.1.254
 ip route 172.16.<BANCADA>0.0 255.255.255.0 172.16.<BANCADA>1.253
 end
-
-#		FALTA ADICIONAR ROUTE PARA 172.16.<BANCADA>0.0/24
-
-#		ADICIONAR CAPACIDADE NAT AO ROUTER
--- FTP downloader/config_scripts/tux1_2.sh
+++ FTP downloader/config_scripts/tux1_2.sh
@@ -24,8 +24,7 @@ arp -d 172.16.<BANCADA>1.253
 #Experiment 4
 #	Make default route to rc
 route add default gw 172.16.<BANCADA>1.254
-
-#		FALTA ADICIONAR ROUTE PARA 172.16.<BANCADA>0.0/24
+route add -net 172.16.<BANCADA>0.0/24 gw 172.16.61.253
 
 echo 0 > /proc/sys/net/ipv4/conf/eth0/accept_redirects
 echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects
